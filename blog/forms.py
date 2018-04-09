af1c34edd5961a02b2dd6355a6b1a4ee30ffbfa8
shivam160677 <shivam160677@gmail.com>
2018-04-09 19:18:15 +0530

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Post, Comment
+
+class UserForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model=User
+        fields=('username','email','password')
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author','text',)
+
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
+        }
---
+++ blog/views.py
@@ -0,0 +1,123 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from blog.forms import CommentForm,PostForm,UserForm
+from blog.models import Post,Comment
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView )
+# Create your views here.
+
+class AboutView(TemplateView):
+    template_name='about.html'
+
+class PostListView(ListView):
+    model=Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model= Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url='/login/'
+    redirect_field_name='blog/post_detail.html'
+    form_class=PostForm
+    model=Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url='/login/'
+    redirect_field_name='blog/post_detail.html'
+    form_class=PostForm
+    model=Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model=Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url='/login/'
+    redirect_field_name='blog/post_list.html'
+    model=Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+#############################
+#Functional views for comments
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
+
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+
+        # Get info from "both" forms
+        # It appears as one form to the user on the .html page
+        user_form = UserForm(data=request.POST)
+
+
+        # Check to see both forms are valid
+        if user_form.is_valid():
+
+            # Save User Form to Database
+            user = user_form.save()
+
+            # Hash the password
+            user.set_password(user.password)
+
+            # Update with Hashed password
+            user.save()
+
+            # Registration Successful!
+            registered = True
+
+        else:
+            # One of the forms was invalid if this else gets called.
+            print(user_form.errors)
+
+    else:
+        # Was not an HTTP post so we just render the forms as blank.
+        user_form = UserForm()
+
+    # This is the render and context dictionary to feed
+    # back to the registration.html file page.
+    return render(request,'registration/registration.html',{'user_form':user_form,'registered':registered})
